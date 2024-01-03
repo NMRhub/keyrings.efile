c2cd63a484f8d1304dbd2b6b55e2b5483ce4903d
Gerard Weatherby <gweatherby@uchc.edu>
2024-01-03 13:09:28 -0500

    Fix EncryptedFile priority property

---
+++ src/tests/test_EncryptedFile.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+import keyring
+import keyrings
+from keyrings.efile import EncryptedFile
+
+
+class TestEncryptedFile(TestCase):
+
+    def setUp(self) -> None:
+        self.ef = EncryptedFile()
+#        self.ef.set_password('a', 'b', 'c')
+
+
+    def test_get_password(self):
+        self.assertEquals(self.ef.get_password('a','b'),'c')
+
+    def test_print(self):
+        kr = keyring.get_keyring()
+        print(kr)
+
